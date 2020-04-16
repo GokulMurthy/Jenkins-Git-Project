138d76afcde0c016527e3f41322d25d25845d066
GokulMurthy <gokulmurthy86@gmail.com>
2020-04-15 21:18:19 -0500

    Change in Locator

--- src/test/java/com/indigo/constantvalues/LocatorValues.java
+++ src/test/java/com/indigo/constantvalues/LocatorValues.java
@@ -6,8 +6,8 @@ public class LocatorValues {
 	public String destinationCity = "NAME:or-dest";
 	public String departureDate = "XPATH://*[@id=\"bookFlightTab\"]/form/div[3]/div[2]/div[1]/div/input";
 	public String returnDate = "NAME:or-return";
-	public String nextButton = "XPATH://*[contains(@id,'1586')]/div/div[2]/div/a";
-	public String monthReturn = "XPATH://*[contains(@id,'1586')]/div/div[1]/div/div/span[1]";
+	public String nextButton = "XPATH://*[contains(@id,'15')]/div/div[2]/div/a";
+	public String monthReturn = "XPATH://*[contains(@id,'15')]/div/div[1]/div/div/span[1]";
 	public String yearReturn = "XPATH://div[@class='ui-datepicker-header ui-widget-header ui-helper-clearfix ui-corner-right']//span[@class='ui-datepicker-year'][contains(text(),'2020')]";
 	public String table = "XPATH://div[@class='ui-datepicker-group ui-datepicker-group-last']//tr";
 	public String searchFlightButton = "XPATH://*[@id=\"bookFlightTab\"]/form/div[7]/div[6]/button/span[1]";
